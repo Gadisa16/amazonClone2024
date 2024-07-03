294117f7e2a0f660f88ced5f455b33dd97e65fd3
Gadisa16 <133113137+Gadisa16@users.noreply.github.com>
2024-07-03 22:23:01 +0300

    update axios baseURL to production endpoint

--- amazon-clone/src/API/axios.js
+++ amazon-clone/src/API/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:5001/clone-df1cc/us-central1/api',
+    // baseURL: 'http://127.0.0.1:5001/clone-df1cc/us-central1/api',
+    baseURL: "https://amazon-deploy-yitn.onrender.com/"
 });
 
 export { axiosInstance }
