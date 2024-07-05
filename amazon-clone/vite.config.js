9135b68b448ec4e398919bd80f587b41eda50c2d
Gadisa16 <133113137+Gadisa16@users.noreply.github.com>
2024-07-05 20:37:48 +0300

    some modification to vite.config.js and package.json is done

--- amazon-clone/vite.config.js
+++ amazon-clone/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/amazonClone2024/amazon-clone/', // Set your desired base path (e.g., '/my-subdirectory/')
 })
